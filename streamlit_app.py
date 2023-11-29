dd01f4adf4c6492a052ac277f648584f9a4b2149
Akshat Agrawal <106049888+akshatagrawal1998@users.noreply.github.com>
2023-11-29 11:05:25 +0530

    Update streamlit_app.py
    read csv file and display it

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,7 @@ streamlit.title("Second title")
 streamlit.header('This is a header')
 streamlit.text("This is a text")
 streamlit.text("This is another text")
+
+import pandas
+df = pd.read_csv(" https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(df)
