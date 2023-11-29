67e2963d95e452a8ce0c8248c7d8fcaee42c8233
Akshat Agrawal <106049888+akshatagrawal1998@users.noreply.github.com>
2023-11-29 10:56:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,6 @@ import streamlit
 
 streamlit.title("First Streamlit App Title for Practice")
 streamlit.title("Second title")
+streamlit.header('This is a header')
+streamlit.text("This is a text")
+streamlit.text("This is another text")
